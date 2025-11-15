805fe8cf2fe1063cb4e4c8b544eac20f48712427
lottiejudge <lottielucyjudge@gmail.com>
2025-11-15 08:31:11 +0000

    scrabble commit

--- scrabble/scrabble.py
+++ scrabble/scrabble.py
@@ -1,26 +1,33 @@
 import unittest
 
 class ScrabbleTest(unittest.TestCase):
-  def test_converts_the_letter_A(self):
+  def test_converts_a_single_letter(self):
      self.assertEqual( scrabble_point_convertor('A'), 1)
-    
-  def test_converts_the_letter_A_twice(self):
-     self.assertEqual( scrabble_point_convertor('AA'), 2)
-  
-  def test_converts_the_letter_B(self):
      self.assertEqual( scrabble_point_convertor('B'), 3)
+     self.assertEqual( scrabble_point_convertor('Z'), 10)
+    
+  def test_converts_multiple_letters_no_added_rules(self):
+     self.assertEqual( scrabble_point_convertor('AI'), 2)
+     self.assertEqual( scrabble_point_convertor('ABX'), 12)
+  
+  def test_converts_double_letter(self):
+     self.assertEqual(scrabble_point_convertor('W*'), 8)
+
 
 
 def scrabble_point_convertor(word):
    letter_to_points = {
-      'A': 1, 'B': 3
+      'A': 1, 'B': 3, 'C':3, 'D': 2,'E':1,'F':4,'G':2,'H':4, 'I':1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 4, 'O': 1, 'P':3, 'Q': 10,'R':1,'S': 1,'T': 1, 'U': 1, 'V':4, 'W':4, 'X':8, 'Y':4, 'Z': 10, "": 0,
    }
    result = 0
-  
 
+   for i, letter in enumerate(word):
+      if letter == '*':
+        result += letter_to_points[word[i-1 *2]]
+        print(result)
+      result += letter_to_points[letter] 
 
    return result
-
 
 
 
